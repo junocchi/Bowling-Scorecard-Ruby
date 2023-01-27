7a29a5358690186d9d4977e4c4748d84e23e6d1d
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-01-27 17:51:08 +0000

    Updated code and tests

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,41 +1,38 @@
 class Bowling
-  attr_reader :score, :current_frame
+  attr_reader :score
 
   def initialize
     @score = 0
-    @current_frame = 1
+    @frame = 1
     @bonus_roll = 0
   end
 
   def process_frame(rolls)
-    fail "Game over." if @current_frame > 10
+    raise 'Game over.' if @frame > 10
 
-    # Calculate roll score
-    rolls.each do |roll|
+    pins = 10
+
+    # Calculates roll score
+    rolls.each_with_index do |roll, index|
+      pins -= roll
       @score += roll
 
-      # Calculates num of strikes
-      consecutive_strikes = (roll == 10) && (@bonus_roll == 2)
-      multiplier = consecutive_strikes ? 2 : 1
-    
       # Apply bonus from previous roll
       if @bonus_roll > 0
-        @score += roll * multiplier
+        consecutive_strikes = ((roll == 10) && (@bonus_roll == 2))
+        bonus_multiplier = (consecutive_strikes ? 2 : 1)
+        @score += (roll * bonus_multiplier)
         @bonus_roll -= 1
+      end
+
+      # Set bonus in case of strike or spare
+      if (pins == 0) && (@frame < 10)
+        @bonus_roll = (2 - index)
+        break
       end
     end
 
-    # Update bonus for next roll
-    @bonus_roll = 
-      if rolls[0] == 10
-        2
-      elsif rolls[0] + rolls[1] == 10
-        1
-      else
-        0
-      end
-
-    @current_frame += 1
+    @frame += 1
   end
 
   def report_score
@@ -48,3 +45,43 @@ class Bowling
     end
   end
 end
+
+## Tests
+## ... describe, it
+bowling = Bowling.new
+bowling.process_frame([6, 4])
+bowling.process_frame([1, 0])
+puts "#{bowling.score} should be 12"
+
+bowling = Bowling.new
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([5, 3])
+puts "#{bowling.score} should be 56"
+## ...
+
+bowling = Bowling.new
+bowling.process_frame([1, 4])
+bowling.process_frame([4, 5])
+bowling.process_frame([6, 4])
+bowling.process_frame([5, 5])
+bowling.process_frame([10, 0])
+bowling.process_frame([0, 1])
+bowling.process_frame([7, 3])
+bowling.process_frame([6, 4])
+bowling.process_frame([10, 0])
+bowling.process_frame([2, 8, 6])
+puts "#{bowling.score} should be 133"
+
+bowling = Bowling.new
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10])
+bowling.process_frame([10, 10])
+puts "#{bowling.score} should be 300"
